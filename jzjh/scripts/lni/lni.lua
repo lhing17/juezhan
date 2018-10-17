37ef431bb2f92c85f16eaf1f6741672e007d67cd
lhing17 <lhing17@163.com>
2018-10-17 17:38:43 +0800

    引入lni-loader

---
+++ jzjh/scripts/lni/lni.lua
@@ -0,0 +1,19 @@
+local lni = require 'ac.lni-loader'
+local storm = require 'jass.storm'
+
+ac.lni = {}
+
+lni:set_marco('TableSearcher', '$MapPath$lni\\table\\')
+for _, path in ipairs(ac.split(package.path, ';')) do
+	-- 把packagepath的?.lua替换成lni\table\.iniconfig
+	local buf = storm.load(path:gsub('%?%.lua', 'lni\\table\\.iniconfig'))
+	-- 如果找到了.iniconfig文件，将当前路径设置为地图搜索路径
+	if buf then
+		lni:set_marco('MapPath', path:gsub('%?%.lua', ''))
+		break
+	end
+end
+
+function ac.lni_loader(name)
+	ac.lni[name] = lni:packager(name, storm.load)
+end
